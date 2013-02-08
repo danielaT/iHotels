3e27dcbebad12b3080393ac6dfa45933b4dd0413
Daniela <daniela.todorova89@gmail.com>
2013-02-08 14:40:11 +0200

    dfdsfdsffds

--- iHotels v1.0/iHotels v1.0/DetailViewController.m
+++ iHotels v1.0/iHotels v1.0/DetailViewController.m
@@ -4,7 +4,7 @@
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
 //  DetailViewController.m
 //  iHotels v1.0
-//
+//gfgfgfdgdf
 //  Created by Student14 on 1/30/13.
 //  Copyright (c) 2013 Student14. All rights reserved.
 //
