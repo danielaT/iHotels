7cfd880f4aac11723f98b1000b34dd04239f0896
Daniela <daniela.todorova89@gmail.com>
2013-02-08 16:34:06 +0200

    iuhu

--- iHotels v1.0/iHotels v1.0/DetailViewController.m
+++ iHotels v1.0/iHotels v1.0/DetailViewController.m
@@ -1,13 +1,7 @@
-
-// komentar ot Marto
-
-// dani's comment
-
 //  DetailViewController.m
 //  iHotels v1.0
 //  Created by Student14 on 1/30/13.
 //  Copyright (c) 2013 Student14. All rights reserved.
-//
 
 #import "DetailViewController.h"
 
